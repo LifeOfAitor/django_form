ef26c2b10e05bc139aeb33405572f1723cad0c7f
Aitor <aitorgaillard99@gmail.com>
2024-09-25 07:38:34 +0200

    send confirmation email to user

--- job_application/views.py
+++ job_application/views.py
@@ -1,7 +1,9 @@
+from django.dispatch import receiver
 from django.shortcuts import render
 from .forms import ContactForm
 from .models import Form
 from django.contrib import messages
+from django.core.mail import EmailMessage
 
 
 # redirects to index.html when the server launches
@@ -19,11 +21,25 @@ def index(request):
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
 
-            # store data on the database#
+            # store data on the database
             Form.objects.create(first_name=first_name, last_name=last_name,
                                 email=email, date=date, occupation=occupation)
+
+            # send email with confirmation
+            send_mail(first_name, email)
+
             # show message on index when form sent correctly
+            messages.success(request, "form submitted successfully")
         else:
             # This will print any form validation errors
             print(form.errors)
     return render(request, "index.html")
+
+
+def send_mail(name, user_email):
+    message_body = f"""
+        A new job application was submitted. Thank you {name}.
+    """
+    email_message = EmailMessage("Form submission confirmation",
+                                 message_body, to=[user_email])
+    email_message.send()
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+# to use .env file´s data
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# use email backend
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
