34cf968f20eee9f6aed23d0c79c4e98d25cccafa
Aitor <aitorgaillard99@gmail.com>
2024-09-28 10:29:07 +0200

    add admin interface

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Form
 
-# Register your models here.
+class FormAdmin(admin.ModelAdmin):
+    # modify the parameters to make interface more customizable, showing more
+    # relevant data or adding search_fields for example
+    list_display = ("first_name", "last_name", "email")
+    search_fields = ("first_name", "last_name")
+    list_filter = ("date")
+    ordering = ("first_name")
+
+admin.site.register(Form, FormAdmin)
